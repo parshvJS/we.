5facb5a9ec1202eb0f451a61f87cf756e4fc2b5b
parshvJS <parshvsheth2006@gmail.com>
2023-12-12 23:13:34 +0530

    userProfile Implement/feat

--- src/Pages/UserProfile.jsx
+++ src/Pages/UserProfile.jsx
@@ -8,7 +8,11 @@ const UserProfile = () => {
         const user=await getUserProfile(id)
     },[])
   return (
-    <div>UserProfile</div>
+<div className="max-w-[819px] mx-auto sm:w-[600px] md:w-[700px] lg:w-[800px] xl:w-[819px] h-[416px] bg-red-200 rounded-[15px]">
+
+<div class="w-[170.19px] h-[164.11px] bg-rose-600 rounded-full sm:w-[200px] md:w-[250px] lg:w-[300px] xl:w-[350px]"></div>
+
+</div>
   )
 }
 
